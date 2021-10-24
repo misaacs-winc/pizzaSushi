f86e94def2840f0669319fe3a19fcbb5ce7fb9c3
misaacs-winc <88142732+misaacs-winc@users.noreply.github.com>
2021-10-24 15:58:17 +0200

    Add file by upload

---
+++ pizza.js
@@ -0,0 +1,26 @@
+const makePizza = function () {
+    console.log("1. Preheat pizza stone (or pizza pan or baking sheet):");
+    console.log("2. Divide the dough into two balls:");
+    console.log("3. Prep toppings:");
+    console.log("4. Flatten dough ball, and stretch out into a round:");
+    console.log("5. Brush dough top with olive oil:");
+    console.log("6. Spread with tomato sauce and sprinkle with toppings:");
+    console.log("8. Bake pizza:");
+}
+
+const makeSushi = function () {
+    console.log("1. Prep the Rice & Nori");
+    console.log("2. Spread the Rice on the Nori.");
+    console.log("3. Add Fillings to the Bottom Quarter of the Nori");
+    console.log("4. Roll the Sushi Part Way");
+    console.log("5. Roll the Sushi the Rest of the Way");
+    console.log("6. Move to Cutting Board.");
+    console.log("7. Cut the Sushi. ");
+    console.log("8. Serve");
+};
+
+makeSushi();
+makeSushi();
+makeSushi();
+makePizza();
+makePizza();
